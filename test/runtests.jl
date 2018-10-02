0282fb280da50c84c9f487dafaa98dd4d6523820
Karl Dyrhage <karl.dyrhage@icm.uu.se>
2018-10-02 15:02:12 +0200

    Added more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,10 @@ using Test
     @test length(chr) == 1
     chr = chr[1]
 
+    @testset "Extended methods" begin
+        @test length(chr.genes[1]) == length(sequence(chr.genes[1]))
+    end
+
     @testset "Gene properties" begin
         @test chr.genes[2].locus_tag == "tag01"
         @test (chr.genes[2].locus_tag = "tag01") == "tag01"
@@ -14,6 +18,7 @@ using Test
             chr.genes[2].test = "New column"
             chr.genes[2].test == "New column" && all(ismissing, chr.genes[[1,3,4,5,6]].test)
         end
+        @test get(chr.genes[1], :locus_tag, "") == ""
         @test get(chr.genes[2], :locus_tag, "") == "tag01"
         @test get(chr.genes[2], :locustag, "") == ""
         @test begin
@@ -38,6 +43,19 @@ using Test
         @test @genes(chr, iscomplement(gene)) == chr.genes[[5,6]]
         @test @genes(chr, :feature == "CDS", !iscomplement(gene)) == chr.genes[[2,4]]
         @test @genes(chr, length(gene) < 300)[1] == chr.genes[2]
+        @test length(@genes(chr, get(gene, :locus_tag, "") == "")) == 3
+    end
+
+    @testset "Broadcast" begin
+        # Broadcasted assignment on existing property
+        chr.genes[3:4].gene .= "AXL2P"
+        @test all(chr.genes[3:4].gene .== "AXL2P")
+        # Broadcasted assignment on previously missing property
+        chr.genes[3:4].newproperty .= true
+        @test all(chr.genes[3:4].newproperty .== true)
+        # Broadcasted assignment with @genes
+        @genes(chr, :feature == "gene").newproperty .= false
+        @test all(chr.genes[[3,5]].newproperty .== false)
     end
 
     @testset "Locus" begin
@@ -45,9 +63,11 @@ using Test
         @test Locus() == locus
         @test chr.genes[2].locus < chr.genes[4].locus
         @test chr.genes[2].locus == chr.genes[2].locus
+        @test iscomplement(chr.genes[2]) == false
+        @test iscomplement(chr.genes[5]) == true
     end
 
     seq = dna"atgtccatatacaacggtatctccacctcaggtttagatctcaacaacggaaccattgccgacatgagacagttaggtatcgtcgagagttacaagctaaaacgagcagtagtcagctctgcatctgaagccgctgaagttctactaagggtggataacatcatccgtgcaagaccaagaaccgccaatagacaacatatgtaa"
-    @test genesequence(chr.genes[2]) == seq
+    @test sequence(chr.genes[2]) == seq
     @test length(chr.genes[2]) == length(seq)
 end
