fd0c1979f7e24a7d51b99f210195c88861708365
Karl Dyrhage <karl.dyrhage@icm.uu.se>
2020-01-23 16:42:59 +0100

    Added GFF parser

--- src/readgbk.jl
+++ src/readgbk.jl
@@ -208,3 +208,166 @@ function readgbk(input::IO, G::Type = Gene; gunzip = false)
 	end
 	chrs
 end
+
+
+
+function parsechromosome_gff(lines, G)
+	chrs = Chromosome{G}[]
+	chr = Chromosome{G}()
+    genes = G[]
+    iobuffer = IOBuffer()
+	isheader = true
+    isfooter = false
+    qualifier = String("")
+    content = String("")
+    header = IOBuffer()
+	currentchr = 0
+	currentfasta = ""
+	for (linecount, line) in enumerate(lines)
+		# Store the header
+		if line[1:2] == "##" && line != "##FASTA"
+			isheader = true
+			println(header, line)
+		else
+			isheader = false
+		end
+		if line == "##FASTA"
+			isfooter = true
+		elseif !isfooter && !isheader
+			(seqid, source, feature, sstart, send, score, strand, phase, attributes) = split(line, '\t')
+			if seqid ∉ [chr.name for chr in chrs]
+				chr = Chromosome{G}()
+				push!(chrs, chr)
+				chr.name = seqid
+				chr.genedata[!, :source] = Union{Missing, String}[]
+				chr.genedata[!, :score] = Union{Missing, Float64}[]
+				chr.genedata[!, :phase] = Union{Missing, Int}[]
+			end
+			locus = Locus(parse(Int, sstart):parse(Int, send), strand[1])
+			addgene!(chr, Symbol(feature), locus)
+			if source != "." && ismissing(chr.genes[end].source)
+				chr.genes[end].source = source
+				# pushproperty!(chr.genes[end], :source, String(source))
+			end
+			if score != "." && ismissing(chr.genes[end].score)
+				chr.genes[end].score = parse(Float64, score)
+				# pushproperty!(chr.genes[end], :score, String(score))
+			end
+			if phase != "."
+				chr.genes[end].phase = parse(Int, phase)
+			end
+			if attributes != "."
+				for attribute in split(attributes, ';')
+					qualifier, value = split(attribute, '=')
+					pushproperty!(chr.genes[end], Symbol(qualifier), String(value))
+				end
+			end
+		elseif isfooter
+			if line[1] == '>'
+				currentfasta = line[2:end]
+				if currentchr > 0
+					seq = String(take!(iobuffer))
+					chrs[currentchr].sequence = LongDNASeq(seq)
+				end
+				currentchr = findfirst(chr -> chr.name == currentfasta, chrs)
+			else
+				if line[1] ∉ ['A', 'T', 'G', 'C']
+					println(line)
+				end
+				print(iobuffer, line)
+			end
+		end
+	end
+	chrs[findfirst(chr -> chr.name == currentfasta, chrs)].sequence = LongDNASeq(String(take!(iobuffer)))
+	headerstring = String(take!(header))
+	for chr in chrs
+		chr.header = headerstring
+	end
+	return chrs
+end
+
+
+function readgff(filename::AbstractString, G::Type = Gene; gunzip = false)
+	gz = filename[end-2:end] == "gz"
+	if gz || gunzip
+		GZip.open(f -> readgff(f, G), filename)
+	else
+		open(f -> readgff(f, G), filename)
+	end
+end
+
+function readgff(input::IO, G::Type; gunzip = false)
+	finished = false
+	chrs = Chromosome{G}[]
+	if gunzip
+		lines = readlines(gzdopen(input))
+	else
+		lines = readlines(input)
+	end
+	currentline = 1
+	# while !finished
+	# 	if currentline >= length(lines)
+	# 		break
+	# 	end
+	# 	i, chr = parsechromosome_gff(lines[currentline:end], G)
+	# 	currentline += i
+	# 	push!(chrs, chr)
+	# end
+	parsechromosome_gff(lines, G)
+end
+
+
+function gffstring(gene::Gene)
+	buf = IOBuffer()
+	firstattribute = true
+    for field in names(parent(gene).genedata)
+        field in [:source, :score, :phase] && continue
+        v = parent(gene).genedata[index(gene), field]
+        if !ismissing(v)
+			if firstattribute
+				firstattribute = false
+			else
+				print(buf, ";")
+			end
+            if v isa AbstractVector
+                for i in eachindex(v)
+					print(buf, field, "=", v[i])
+					i != lastindex(v) && print(buf, ";")
+                end
+            else
+				print(buf, field, "=", v)
+            end
+        end
+    end
+	join([parent(gene).name,
+		get(gene, :source, "."),
+		feature(gene),
+		locus(gene).position.start,
+		locus(gene).position.stop,
+		get(gene, :score, "."),
+		locus(gene).strand,
+		get(gene, :phase, "."),
+		String(take!(buf))], '\t')
+end
+
+printgff(io::IO, chr::Chromosome) = printgff(io, [chr])
+function printgff(io::IO, chrs::AbstractVector{Chromosome{Gene}})
+	iobuffer = IOBuffer()
+	### Header
+	print(iobuffer,  chrs[1].header)
+	### Body
+	for chr in chrs
+		for gene in chr.genes
+			println(iobuffer, gffstring(gene))
+		end
+	end
+	### Footer
+	println(iobuffer, "##FASTA")
+	for chr in chrs
+		println(iobuffer, ">", chr.name)
+		for s in Iterators.partition(chr.sequence, 80)
+			println(iobuffer, join(s))
+		end
+	end
+	print(io, String(take!(iobuffer)))
+end
