3c6131b240b0ed3498b85265832db263b04768b2
Karl Dyrhage <karl.dyrhage@gmail.com>
2021-04-06 13:56:22 +0200

    Added GFF.Writer docstring

--- src/GFF/writer.jl
+++ src/GFF/writer.jl
@@ -8,6 +8,16 @@ function BioGenerics.IO.stream(writer::Writer)
     return writer.output
 end
 
+"""
+    GFF.Writer(output::IO; width=70)
+
+Create a data writer of the GFF file format.
+```julia
+open(GFF.Writer, outfile) do writer
+    write(writer, genome)
+end
+```
+"""
 function Writer(output::IO)
     if output isa TranscodingStream
         return Writer{typeof(output)}(stream)
